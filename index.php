838692e2c41ba54515c94ed942528bcf8b62c9c7
Imamady <awrilecx@gmail.com>
2023-11-27 15:00:32 +0700

    pertemuan8

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compitable" content="IE=edge"> 
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0"> 
+    <title>Document</title>
+</head>
+<body>
+    <h1>Yayasan Pondok Pesantren</h1>
+    <h2>SMK Ponpes Al azhar</h2>
+    <a href="data_Santri.php">Daftar Santri</a><br>
+    <a href="tambah_santri.php">pendaftaran</a>
+</body>
+</html>
+      
